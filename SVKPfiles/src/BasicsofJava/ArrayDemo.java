12d4830aa4ead8c43211e91c0636b5f2de698008
user <user@192.168.1.73>
2024-11-05 14:43:20 +0530

    fourth commit

--- SVKPfiles/src/BasicsofJava/ArrayDemo.java
+++ SVKPfiles/src/BasicsofJava/ArrayDemo.java
@@ -3,7 +3,7 @@ package BasicsofJava;
 public class ArrayDemo {
 
 	public static void main(String[] args) {
-	int num[] = {2,3,4}
+	int num[] = {2,3,4};
 	System.out.println(num[0]);
 	System.out.println(num[2]);
 
