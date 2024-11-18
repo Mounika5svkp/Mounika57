b86c6a4df40917f52cd09a41f0f6a9d0dc2769a4
user <user@10.0.0.101>
2024-11-18 13:26:24 +0530

    third commit

--- SVKPfiles/src/BasicsofJava/ifelseifladder.java
+++ SVKPfiles/src/BasicsofJava/ifelseifladder.java
@@ -1,22 +1,22 @@
 package BasicsofJava;
 
-public class ifelseifladder {
+public class IfelseIfLadder {
 
 	public static void main(String[] args) {
 		int Score =89;
-		if (score>90)
+		if(score>90)
 		{
 			System.out.println("grade a");
 		}
-		else if (Score>=80)
+		else if(Score>=80)
 		{
 			System.out.println("grade b");
 		}
-		else if (score >=70)
+		else if(score>=70)
 		{
 			System.out.println("gradec");
 		}
-		else if (score>=60)
+		else if(score>=60)
 		{
 			System.out.println("grade d");
 		}
