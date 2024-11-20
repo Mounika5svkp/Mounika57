36bec8e9a199b9c2c252afaa1ab223d4c5439d60
user <user@192.168.1.73>
2024-11-20 15:25:21 +0530

    nineth commit

---
+++ SVKPfiles/src/org/tnsif/capgemini/c2tc/constructor/BeforeConstructor.java
@@ -0,0 +1,20 @@
+package org.tnsif.capgemini.c2tc.constructor;
+
+class Myclass
+{
+
+	 {
+		String color;
+		int cost;
+		String Brand;
+	}
+	public class BeforeConstroctor{
+		public static void main( String[] args) {
+			Myclass obj=new Myclass();
+			System.out.println();
+		}
+	}
+}
+	
+
+
